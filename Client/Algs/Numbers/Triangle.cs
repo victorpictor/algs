89e6125c389d15e16f342d9d318d3055320e2cd9
victor <vrusu@fareportal.com>
2016-01-12 16:54:09 -0500

    triangle

---
+++ Client/Algs/Numbers/Triangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Client.Algs.Numbers
+{
+    public class Triangle
+    {
+        public List<List<int>> Find(int[] a)
+        {
+            if (a == null)
+                throw new ArgumentNullException();
+
+            if (a.Length < 3)
+                return new List<List<int>>();
+
+            var aL = a.Length;
+
+            var result = new List<List<int>>();
+            for (int l = 3; l < aL; l++)
+            {
+                for (int i = 0; i <= l - 3; i++)
+                {
+                    var j = l - 1;
+
+                    for (int k = i + 1; k < l - 1; k++)
+                    {
+                        if (a[i] + a[k] > a[j] &&
+                            a[i] + a[j] > a[k] &&
+                            a[j] + a[k] > a[i])
+                            result.Add(new List<int>() { a[i], a[k], a[j] });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Client/Runner.cs
+++ Client/Runner.cs
@@ -3,50 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Client.Algs.Numbers;
 using Client.Algs.Search;
+using Client.Algs.Strings;
 using Client.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Client
 {
-
-    // foor buur
-
-    public class IsomorphicStrings
-    {
-        public bool Check(string s1, string s2)
-        {
-            if (s1.Length != s2.Length)
-                return false;
-            
-
-            var dic = new Dictionary<char, char>();
-
-            for (int i = 0; i < s1.Length; i++)
-            {
-                if (dic.ContainsKey(s1[i]))
-                {
-                    if (dic[s1[i]] == s2[i])
-                        continue;
-                    
-                    return false;
-                }
-
-                dic.Add(s1[i], s2[i]);
-            }
-
-            return true;
-        }
-    }
-
-
     [TestClass]
     public class Runner
     {
         [TestMethod]
         public void AlgTest()
         {
-            var are = new IsomorphicStrings().Check("foorum", "brrkom");
+            var are = new Triangle().Find(new int[]{9,3,10,7,1,8,10,4});
         }
     }
 }
